a6e9650001c8f8830f7ce02a45cc471d0e12ad6e
David Masek <dmasek@purestorage.com>
2025-06-05 20:04:26 +0200

    01 reformat

--- 01.exs
+++ 01.exs
@@ -6,23 +6,21 @@
 # 3   9
 # 3   3"
 
-IO.puts("A:")
-content |> String.split("\n", trim: true)
-|> Enum.map(fn x -> String.split(x) end)
-|> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
-|> Enum.unzip()
+{l, r} =
+  String.split(content, "\n", trim: true)
+  |> Enum.map(fn x -> String.split(x) end)
+  |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
+  |> Enum.unzip()
+
+{l, r}
 |> then(fn {l, r} -> {Enum.sort(l), Enum.sort(r)} end)
 |> then(fn {l, r} -> Enum.zip(l, r) end)
 |> Enum.reduce(0, fn {a, b}, acc -> acc + abs(a - b) end)
-|> IO.inspect()
+|> IO.inspect(label: "A")
 
-IO.puts("B:")
-{l, r} = content |> String.split("\n", trim: true)
-|> Enum.map(fn x -> String.split(x) end)
-|> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
-|> Enum.unzip()
-|> IO.inspect()
 freq = Enum.frequencies(r)
-l |> Enum.map(fn x -> x * Map.get(freq, x, 0) end)
-|> Enum.sum
-|> IO.inspect()
+
+l
+|> Enum.map(fn x -> x * Map.get(freq, x, 0) end)
+|> Enum.sum()
+|> IO.inspect(label: "B")

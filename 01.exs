77f0c840b098109d9e95a5864b7b1b734596e838
David Masek <dmasek@purestorage.com>
2025-06-05 18:06:15 +0200

    01b

--- 01.exs
+++ 01.exs
@@ -1,4 +1,12 @@
 {:ok, content} = File.read("01.txt")
+# content = "3   4
+# 4   3
+# 2   5
+# 1   3
+# 3   9
+# 3   3"
+
+IO.puts("A:")
 content |> String.split("\n", trim: true)
 |> Enum.map(fn x -> String.split(x) end)
 |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
@@ -8,5 +16,13 @@ content |> String.split("\n", trim: true)
 |> Enum.reduce(0, fn {a, b}, acc -> acc + abs(a - b) end)
 |> IO.inspect()
 
-
-# koriandr a zakysana smetana
+IO.puts("B:")
+{l, r} = content |> String.split("\n", trim: true)
+|> Enum.map(fn x -> String.split(x) end)
+|> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
+|> Enum.unzip()
+|> IO.inspect()
+freq = Enum.frequencies(r)
+l |> Enum.map(fn x -> x * Map.get(freq, x, 0) end)
+|> Enum.sum
+|> IO.inspect()
